36d53efc6bd3170155512ba7276ed77ff61e3342
majkl zumberi <majklzumberi00@gmail.com>
2020-06-27 15:48:18 +0200

    edited dev script running the server with nodemon that restart the server automatically

--- core-http-module/app.js
+++ core-http-module/app.js
@@ -1,7 +1,7 @@
 const http=require("http");
 
 const server=http.createServer((req,res)=>{
-    res.end("il server funziona!");
+    res.end("il server funziona! \ne nodemon riavvia il server automaticamente");
 });
 
 server.listen("3000");
