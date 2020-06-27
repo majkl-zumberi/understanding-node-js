70c978722e93a24a1dc4b4a05bbc37f36a046e23
majkl zumberi <majklzumberi00@gmail.com>
2020-06-27 18:00:49 +0200

    express server with morgan

--- express-api/app.js
+++ express-api/app.js
@@ -1,9 +1,24 @@
 const express=require("express");
+
 const app=express();
+
 const port=3000;
+
 //bring in routes
-const routes=require("./routes/post");
+const postRoutes=require("./routes/post");
+
+//morgan for middleware 
+/**
+ * morgan agisce come middleware 
+ * riporta informazioni come il tipo di accesso e lo status
+ * simile a json-server
+ * esempio GET / 304 5.418 ms
+ */
+const morgan=require("morgan");
+
+app.use(morgan("dev"));
+
+app.use("/",postRoutes.router);
 
 
-app.get("/",routes.getPosts);
 app.listen(port,()=>console.log("a node server is listening on port:",port,`link: http://localhost:${port}`));
